dd45101310f2d50b063e7265cd713548323b8570
gadihok2 <nalingadihoke@gmail.com>
2020-10-27 00:59:29 -0500

    chatbot

---
+++ bot_botv2.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 18:44:01 2020
+
+@author: nalingadihoke
+"""
+import os
+import sys
+
+import requests
+from flask import Flask, request
+
+import nltk
+#nltk.download('wordnet')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import json
+import pickle
+import numpy as np
+from tensorflow import keras
+
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+import random
+import matplotlib.pyplot as plt
+
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+
+data_file = open('q_n_a.json').read()
+intents = json.loads(data_file)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        #tokenize each word
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        
+        #add documents in the corpus
+        documents.append((w, intent['tag']))
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+        #print(classes)
+        #print(w)
+#print(documents)
+
+# lemmatize, lower each word and remove duplicates
+
+words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# sort classes
+classes = sorted(list(set(classes)))
+
+# print json info
+#print (len(documents), "documents")
+#print (len(classes), "classes", classes)
+#print (len(words), "unique lemmatized words", words)
+
+# Pickle dump if using graphical interface
+#pickle.dump(words,open('words.pkl','wb'))
+#pickle.dump(classes,open('classes.pkl','wb'))
+
+'''Model Training
+# create our training data
+training = []
+output = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+
+# training set, bag of words for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # stem each word
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # create our bag of words array
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag
+    output_row = list(output_empty)
+    #print(classes.index(doc[1]))
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+
+# create train and test lists
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+model = Sequential()
+model.add(Dense(8, input_shape=[len(train_x[0],)]))
+model.add(Dense(8))
+model.add(Dense(8))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+history = model.fit(np.array(train_x), np.array(train_y), epochs=1000, batch_size=8)
+
+
+model.save('model_ChatBot.h5')
+'''
+model = load_model('model_ChatBot.h5')
+
+''' Plotting accuracy and loss
+history_dict = history.history
+
+acc = history.history['acc']
+loss = history.history['loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+'''
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+
+# create a data structure to hold user context
+context = {}
+
+ERROR_THRESHOLD = 0.65
+
+
+def classify(sentence):
+    # generate probabilities from the model
+    p = bow(sentence, words)
+
+    d = len(p)
+    f = len(documents) - 2
+    a = np.zeros([f, d])
+    tot = np.vstack((p, a))
+
+    results = model.predict(tot)[0]
+
+    # filter out predictions below a threshold
+    results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+
+def response(sentence, userID, show_details=False):
+    results = classify(sentence)
+    print('Result:', results)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # set context for this intent if necessary
+                    # print(i)
+                    if 'context_set' in i:
+                        if show_details: print('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+                    # check if this intent is contextual and applies to this user's conversation
+                    if not 'context_filter' in i or \
+                            (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print('tag:', i['tag'])
+                        # a random response from the intent
+                        return (random.choice(i['responses']))
+            results.pop(0)
+
+
+'''FLASK APP'''
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+    data = request.get_json()
+    print(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    responseai = response(message_text, sender_id)
+                    send_message(sender_id, responseai)
+
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    print("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
